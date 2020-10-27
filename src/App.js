3659abb86f981ae116687ec9ad6550a74308936b
somprey <somprey@users.noreply.github.com>
2020-10-27 02:29:03 +0000

    login styling

--- src/App.js
+++ src/App.js
@@ -1,59 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
+import Home from "./components/Home";
+import { Login } from "./components/login/Login";
+/*import Signup from "./components/Signup";
 
-const api = {
-  key: "53268a647d290e9f6457193f68699300",
-  base: "https://api.openweathermap.org/data/2.5",
-};
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";*/
 
 function App() {
-  const dateBuilder = (d) => {
-    let months = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
-    ];
-    let days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ];
-
-    let day = days[d.getDay()];
-    let date = d.getDate();
-    let month = months[d.getMonth()];
-    let year = d.getFullYear();
-
-    return `${day} ${date} ${month} ${year}`;
-  };
-
   return (
-    <div className="app">
-      <main>
-        <div className="search-box">
-          <input type="text" className="search-bar" placeholder="Search..." />
-        </div>
-        <div className="location-box">
-          <div className="location">New York City, US</div>
-          <div className="date">{dateBuilder(new Date())}</div>
-        </div>
-        <div className="weather-box">
-          <div className="temp">15°c</div>
-          <div className="weather">Sunny</div>
-        </div>
-      </main>
+    <div className="App">
+      <nav>
+        <Login />
+      </nav>
+
+      <Home />
+
+      <footer>This is a footer</footer>
     </div>
   );
 }
