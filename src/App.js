173fb75bade78cbf7f7db03f5f73429876e53219
somprey <somprey@users.noreply.github.com>
2020-10-29 09:06:21 +0000

    signuo page completed

--- src/App.js
+++ src/App.js
@@ -1,34 +1,30 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom"
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
-import "./App.scss";
+
 import Navigation from "./components/Navigation";
-import './components/style.css'
+import "./components/style.css";
 
 function App() {
   return (
     <BrowserRouter>
-    <Navigation/>
+      <Navigation />
       <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
 
-          <Route path="/" exact>
-              <Home />
-          </Route>
+        <Route path="/signup" exact>
+          <Signup />
+        </Route>
 
-          <Route path="/signup" exact>
-              <Signup />
-          </Route>
-
-          <Route path="/login" exact>
-              <Login />
-          </Route>
-
+        <Route path="/login" exact>
+          <Login />
+        </Route>
       </Switch>
     </BrowserRouter>
-
-
   );
 }
 
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export class Signup extends React.Component {
   constructor(props) {
@@ -7,23 +8,32 @@ export class Signup extends React.Component {
 
   render() {
     return (
-      <div className="base-container" ref={this.props.containerRef}>
-        <div className="header">Signup</div>
-        <div className="content">
-          <div className="form">
-            <div className="form-group">
-              <label htmlFor="username">Username</label>
-              <input type="text" name="username" placeholder="username" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="email">Email</label>
-              <input type="text" name="email" placeholder="email" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="password">Password</label>
-              <input type="text" name="password" placeholder="password" />
-            </div>
+      <div className="card col-lg-4 container  justify-content-center">
+        <div className="card-header h5">Signup</div>
+        <div className="card-body pt-4">
+          <div className="form-group">
+            <label htmlFor="username">Username: </label>
+            <input type="text" name="username" placeholder="username" />
           </div>
+
+          <div className="form-group">
+            <label htmlFor="email">Email: </label>
+            <input type="email" name="email" placeholder="Email" />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="password">Password: </label>
+            <input type="password" name="password" placeholder="Password" />
+          </div>
+
+          <center>
+            <div type="button" className="btn btn-primary">
+              Signup
+            </div>
+          </center>
+        </div>
+        <div className="card-footer">
+          <Link to="/Login">Log in</Link>
         </div>
       </div>
     );
