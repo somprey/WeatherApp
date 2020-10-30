937712a974af48f21d74c8a6c10da852c0c2fa1c
darneltaphil <darneltaphil@gmail.com>
2020-10-30 06:23:35 +0000

    Fixed Kelvin to Celsius

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,13 +2,29 @@ import React, {useContext} from "react";
 import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 import {AuthContext} from "./context/auth-context";
-
+import swal from "sweetalert"
 const Navigation = () => {
 
     const auth = useContext(AuthContext)
 
     const logoutHandler = e => {
-    auth.logout();
+        swal({
+            title: "Do you want to log out?",
+            text: "",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+          })
+          .then((willDelete) => {
+            if (willDelete) {
+            //   swal("Poof! Your imaginary file has been deleted!", {
+            //     icon: "success",
+            //   });
+            auth.logout();
+            } else {
+            //   swal("Your imaginary file is safe!");
+            }
+          });    
 }
     return (
         <Navbar bg='secondary'>
@@ -19,14 +35,16 @@ const Navigation = () => {
                 <Nav.Link ClassName='text-white btn btn-primary'><Link to='/' >Home</Link> </Nav.Link>
 
                 {
-
-!auth.isLoggedIn && ( <Nav.Link><Link to='/login' ClassName='text-white' style={{ textDecoration: 'none' }}> Login</Link></Nav.Link>)
-
+                !auth.isLoggedIn && (
+                     <Nav.Link><Link to='/login' ClassName='text-white' style={{ textDecoration: 'none' }}> Login</Link></Nav.Link>
+                     )
                 }
 
-                {auth.isLoggedIn && ( 
+                {
+                auth.isLoggedIn && (
                 <Nav.Link className='text-white' onClick={logoutHandler}> Log Out </Nav.Link>
-                )}
+                )
+                }
 
                 </Nav>
 
